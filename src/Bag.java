4c522b8b7d272aac4620937a8e2cf544ba3f0abd
Hanjaemo <hjm2530@gmail.com>
2023-11-21 23:22:00 +0900

    Initial commit

---
+++ src/Bag.java
@@ -0,0 +1,47 @@
+public class Bag {
+
+    private Long amount;
+    private Invitation invitation;
+    private Ticket ticket;
+
+    public Bag(Long amount) {
+        this.amount = amount;
+        this.invitation = null;
+    }
+
+    public Bag(Long amount, Invitation invitation) {
+        this.amount = amount;
+        this.invitation = invitation;
+    }
+
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
+
+    private boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public boolean hasTicket() {
+        return ticket != null;
+    }
+
+    private void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+
+    private void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+}
