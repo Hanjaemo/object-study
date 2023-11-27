b89bcbae2ecf13feda56203d9041c4f2a49d545e
Hanjaemo <hjm2530@gmail.com>
2023-11-28 00:26:25 +0900

    불필요한 코드 제거

--- src/ch2/DiscountPolicy.java
+++ src/ch2/DiscountPolicy.java
@@ -1,9 +1,5 @@
 package ch2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public interface DiscountPolicy {
 
     Money calculateDiscountAmount(Screening screening);
