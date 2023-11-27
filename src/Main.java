2ac617014e444c037f1f8ddccd78c82a826c010c
Hanjaemo <hjm2530@gmail.com>
2023-11-28 00:24:59 +0900

    ch2. 객체지향 프로그래밍

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+
+import ch2.AmountDiscountPolicy;
+import ch2.Money;
+import ch2.Movie;
+import ch2.PeriodCondition;
+import ch2.SequenceCondition;
+
 public class Main {
     public static void main(String[] args) {
-
+        Movie avatar = new Movie("아바타",
+                Duration.ofMinutes(120),
+                Money.wons(10000),
+                new AmountDiscountPolicy(Money.wons(800),
+                        new SequenceCondition(1),
+                        new SequenceCondition(10),
+                        new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59)),
+                        new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(20, 59))));
     }
 }
---
+++ src/ch2/DefaultDiscountPolicy.java
@@ -0,0 +1,26 @@
+package ch2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class DefaultDiscountPolicy implements DiscountPolicy {
+
+    private List<DiscountCondition> conditions;
+
+    public DefaultDiscountPolicy(DiscountCondition... conditions) {
+        this.conditions = Arrays.asList(conditions);
+    }
+
+    @Override
+    public Money calculateDiscountAmount(Screening screening) {
+        for (DiscountCondition each : conditions) {
+            if (each.isSatisfiedBy(screening)) {
+                return getDiscountAmount(screening);
+            }
+        }
+
+        return Money.ZERO;
+    }
+
+    abstract protected Money getDiscountAmount(Screening screening);
+}
